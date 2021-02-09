babeb281ea72f9477afaee6532d58feb3f03754e
annakvach <70780985+annakvach@users.noreply.github.com>
2021-02-10 00:41:35 +0300

    Update filter.py
    > 140
    
    if "--min_length"

--- filter.py
+++ filter.py
@@ -137,10 +137,11 @@ def gc_count(read):
 # ez
 # check for good reads
 def passed(read, int_min_length, float_left_gc_bound, float_right_gc_bound):
-    if len(read) < int_min_length:  # if l(read) < min length => F
-        return False
-    else:
-        return True
+    if "--min_length" in list_flags_and_options:
+        if len(read) < int_min_length:  # if l(read) < min length => F
+            return False
+        else:
+            return True
 
     if "--gc_bounds" in list_flags_and_options:
         less = gc_count(read) < float_left_gc_bound # if less than left GC bound => True
